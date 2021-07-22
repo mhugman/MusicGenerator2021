b0b0adb88113f1153d6349a44108f52f44736709
mhugman <michael.hugman@gmail.com>
2021-07-21 21:45:11 -0500

    added drum fn

--- MusicComposition2021.py
+++ MusicComposition2021.py
@@ -4,113 +4,246 @@
 import numpy as np
 import itertools
 
-# Step 1 - Generate libraries
+np.set_printoptions(linewidth=150)
 
-# 12 possible keys
-# 0: C
-# 1: C# / Db
-# 2: D
-# 3: D# / Eb
-# 4: E
-# 5: F
-# 6: F# / Gb
-# 7: G
-# 8: G# / Ab
-# 9: A
-# 10: A# / Bb
-# 11: B
+HI_HAT_PROB = 0.5
+KICK_PROB = 0.5
+SNARE_PROB = 0.1
+TOM_H_PROB = 0.1
+TOM_M_PROB = 0.1
+TOM_L_PROB = 0.1
 
-# the more key changes, the more complex the song
-# the larger jumps between key changes, the more complex the song
-# separate button to adjust this?
-keyLibrary = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+FILL_HIHAT_PROB = 0.25
+FILL_SNARE_PROB = 0.25
+FILL_TOM_H_PROB = 0.25
+FILL_TOM_M_PROB = 0.25
+FILL_TOM_L_PROB = 0.25
+FILL_KICK_PROB = 0.25
+FILL_CRASH_PROB = 0.25
 
-# rhythm without duration, for 8, 16, and 32 beats
-percussiveRhythmLibrary_8 = list(itertools.product([0, 1], repeat=8))
-percussiveRhythmLibrary_16 = list(itertools.product([0, 1], repeat=16))
-percussiveRhythmLibrary_32 = list(itertools.product([0, 1], repeat=32))
+FILL_LENGTH = 4
 
-# rhythm with duration, for 8, 16, and 32 beats
-# note starts on 2, continues with 1, and ends / doesn't play on 0
-durationRhythmLibrary_8 = list(itertools.product([0, 1, 2], repeat=8))
-durationRhythmLibrary_16 = list(itertools.product([0, 1, 2], repeat=16))
-durationRhythmLibrary_32 = list(itertools.product([0, 1, 2], repeat=32))
+HI_HAT_REPEATED = True
+KICK_REPEATED = True
+KICK_ONE_THREE = True
+TOM_REPEATED = True
+SNARE_BACKBEAT = True
+SNARE_REPEATED = True
+NO_KICK_WHEN_SNARE = True
 
-# Selects notes from a chord with 3 intervals
-melodyLibrary_3_8 = list(itertools.product([0, 1, 2], repeat=8))
-melodyLibrary_3_16 = list(itertools.product([0, 1, 2], repeat=16))
-melodyLibrary_3_32 = list(itertools.product([0, 1, 2], repeat=32))
+# 0 = rest
+# 1 = closed
+# 2 = open
 
-# Selects notes from a chord with 4 intervals
-melodyLibrary_4_8 = list(itertools.product([0, 1, 2, 3], repeat=8))
-melodyLibrary_4_16 = list(itertools.product([0, 1, 2, 3], repeat=16))
-melodyLibrary_4_32 = list(itertools.product([0, 1, 2, 3], repeat=32))
+if HI_HAT_REPEATED: 
+	micro_rhythm_hihat_A = np.random.binomial(2, HI_HAT_PROB, size=(1,4))
+	micro_rhythm_hihat_B = np.random.binomial(2, HI_HAT_PROB, size=(1,4))
+	micro_rhythm_hihat_C = np.random.binomial(2, HI_HAT_PROB, size=(1,4))
+	micro_rhythm_hihat_D = np.random.binomial(2, HI_HAT_PROB, size=(1,4))
 
-# Selects notes from a chord with 5 intervals
-melodyLibrary_5_8 = list(itertools.product([0, 1, 2, 3, 4], repeat=8))
-melodyLibrary_5_16 = list(itertools.product([0, 1, 2, 3, 4], repeat=16))
-melodyLibrary_5_32 = list(itertools.product([0, 1, 2, 3, 4], repeat=32))
+	base_rhythm_hihat_A = np.concatenate((micro_rhythm_hihat_A, micro_rhythm_hihat_A, micro_rhythm_hihat_A, micro_rhythm_hihat_A), axis=1)
+	base_rhythm_hihat_B = np.concatenate((micro_rhythm_hihat_B, micro_rhythm_hihat_B, micro_rhythm_hihat_B, micro_rhythm_hihat_B), axis=1)
+	base_rhythm_hihat_C = np.concatenate((micro_rhythm_hihat_C, micro_rhythm_hihat_C, micro_rhythm_hihat_C, micro_rhythm_hihat_C), axis=1)
+	base_rhythm_hihat_D = np.concatenate((micro_rhythm_hihat_D, micro_rhythm_hihat_D, micro_rhythm_hihat_D, micro_rhythm_hihat_D), axis=1)
 
-# Selects notes from a chord with 6 intervals
-# the more interval jumps, the higher the complexity
-melodyLibrary_6_8 = list(itertools.product([0, 1, 2, 3, 4. 5], repeat=8))
-melodyLibrary_6_16 = list(itertools.product([0, 1, 2, 3, 4, 5], repeat=16))
-melodyLibrary_6_32 = list(itertools.product([0, 1, 2, 3, 4, 5], repeat=32))
+else: 
+	base_rhythm_hihat_A = np.random.binomial(2, HI_HAT_PROB, size=(1,16))
+	base_rhythm_hihat_B = np.random.binomial(2, HI_HAT_PROB, size=(1,16))
+	base_rhythm_hihat_C = np.random.binomial(2, HI_HAT_PROB, size=(1,16))
+	base_rhythm_hihat_D = np.random.binomial(2, HI_HAT_PROB, size=(1,16))
 
-# Chord progressions 
-# 0: I
-# 1: ii
-# 2: iii
-# 3: IV
-# 4: V
-# 5: vi
-# 6: bVII
-# 7: bVI
-# 8: bIII
-# 9: II
-# 10: III
-# 11: iv
-# 12: bV
-# 13: v
-# the higher the numbers, the more the complexity rating
-# the more chord changes, the higher the complexity
-chordProgressionLibrary_4 = list(itertools.product([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13], repeat=4))
-chordProgressionLibrary_8 = list(itertools.product([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13], repeat=8))
-chordProgressionLibrary_16 = list(itertools.product([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13], repeat=16))
+if KICK_REPEATED: 
 
-# Chord extensions
-# 0: Major
-# 1: Minor
-# 2: 5
-# 3: 6
-# 4: 6/9
-# 5: 7
-# 6: add9
-# 7: maj7
-# 8: maj9
-# 9: maj11
-# 10: maj13
-# 11: sus2
-# 12: sus4
-# 13: augmented
-# 14: diminished
-# 15: m6
-# 16: m6/9
-# 17: m7
-# 18: m9
-# 19: madd9
-# 20: m11
-# 21: m13
-# 22: mmaj7
-# 23: mmaj9
-chordExtensionLibrary_4 = list(itertools.product([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23], repeat=4))
-chordExtensionLibrary_8 = list(itertools.product([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23], repeat=8))
-chordExtensionLibrary_16 = list(itertools.product([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23], repeat=16))
+	micro_rhythm_kick_A = np.random.binomial(1, KICK_PROB, size=(1,8))
+	micro_rhythm_kick_B = np.random.binomial(1, KICK_PROB, size=(1,8))
+	micro_rhythm_kick_C = np.random.binomial(1, KICK_PROB, size=(1,8))
+	micro_rhythm_kick_D = np.random.binomial(1, KICK_PROB, size=(1,8))
 
-# From 0 (softest) to 9 (loudest)
-# the more dynamics changes the more complex
-dynamicsLibrary_8 = list(itertools.product([0, 1, 2, 3, 4. 5, 6, 7, 8, 9], repeat=8))
-dynamicsLibrary_16 = list(itertools.product([0, 1, 2, 3, 4. 5, 6, 7, 8, 9], repeat=16))
-dynamicsLibrary_32 = list(itertools.product([0, 1, 2, 3, 4. 5, 6, 7, 8, 9], repeat=32))
+	base_rhythm_kick_A = np.concatenate((micro_rhythm_kick_A, micro_rhythm_kick_A), axis=1)
+	base_rhythm_kick_B = np.concatenate((micro_rhythm_kick_B, micro_rhythm_kick_B), axis=1)
+	base_rhythm_kick_C = np.concatenate((micro_rhythm_kick_C, micro_rhythm_kick_C), axis=1)
+	base_rhythm_kick_D = np.concatenate((micro_rhythm_kick_D, micro_rhythm_kick_D), axis=1)
+
+else: 
+	base_rhythm_kick_A = np.random.binomial(1, KICK_PROB, size=(1,16))
+	base_rhythm_kick_B = np.random.binomial(1, KICK_PROB, size=(1,16))
+	base_rhythm_kick_C = np.random.binomial(1, KICK_PROB, size=(1,16))
+	base_rhythm_kick_D = np.random.binomial(1, KICK_PROB, size=(1,16))
+
+if KICK_ONE_THREE: 
+	np.put(base_rhythm_kick_A[0], 0, 1)
+	np.put(base_rhythm_kick_A[0], 8, 1)
+	np.put(base_rhythm_kick_B[0], 0, 1)
+	np.put(base_rhythm_kick_B[0], 8, 1)
+	np.put(base_rhythm_kick_C[0], 0, 1)
+	np.put(base_rhythm_kick_C[0], 8, 1)
+	np.put(base_rhythm_kick_D[0], 0, 1)
+	np.put(base_rhythm_kick_D[0], 8, 1)
+
+if SNARE_REPEATED: 
+
+	micro_rhythm_snare_A = np.random.binomial(1, SNARE_PROB, size=(1,8))
+	micro_rhythm_snare_B = np.random.binomial(1, SNARE_PROB, size=(1,8))
+	micro_rhythm_snare_C = np.random.binomial(1, SNARE_PROB, size=(1,8))
+	micro_rhythm_snare_D = np.random.binomial(1, SNARE_PROB, size=(1,8))
+
+	base_rhythm_snare_A = np.concatenate((micro_rhythm_snare_A, micro_rhythm_snare_A), axis=1)
+	base_rhythm_snare_B = np.concatenate((micro_rhythm_snare_B, micro_rhythm_snare_B), axis=1)
+	base_rhythm_snare_C = np.concatenate((micro_rhythm_snare_C, micro_rhythm_snare_C), axis=1)
+	base_rhythm_snare_D = np.concatenate((micro_rhythm_snare_D, micro_rhythm_snare_D), axis=1)
+
+else: 
+
+	base_rhythm_snare_A = np.random.binomial(1, SNARE_PROB, size=(1,16))
+	base_rhythm_snare_B = np.random.binomial(1, SNARE_PROB, size=(1,16))
+	base_rhythm_snare_C = np.random.binomial(1, SNARE_PROB, size=(1,16))
+	base_rhythm_snare_D = np.random.binomial(1, SNARE_PROB, size=(1,16))
+
+if SNARE_BACKBEAT: 
+	np.put(base_rhythm_snare_A[0], 4, 1)
+	np.put(base_rhythm_snare_A[0], 12, 1)
+	np.put(base_rhythm_snare_B[0], 4, 1)
+	np.put(base_rhythm_snare_B[0], 12, 1)
+	np.put(base_rhythm_snare_C[0], 4, 1)
+	np.put(base_rhythm_snare_C[0], 12, 1)
+	np.put(base_rhythm_snare_D[0], 4, 1)
+	np.put(base_rhythm_snare_D[0], 12, 1)
+
+if NO_KICK_WHEN_SNARE:
+	indices = np.where(base_rhythm_snare_A[0]>0.9)
+	base_rhythm_kick_A[0][indices] = 0
+
+	indices = np.where(base_rhythm_snare_B[0]>0.9)
+	base_rhythm_kick_B[0][indices] = 0
+
+	indices = np.where(base_rhythm_snare_C[0]>0.9)
+	base_rhythm_kick_C[0][indices] = 0
+
+	indices = np.where(base_rhythm_snare_D[0]>0.9)
+	base_rhythm_kick_D[0][indices] = 0
+
+if TOM_REPEATED: 
+
+	micro_rhythm_tom_h_A = np.random.binomial(1, TOM_H_PROB, size=(1,8))
+	micro_rhythm_tom_h_B = np.random.binomial(1, TOM_H_PROB, size=(1,8))
+	micro_rhythm_tom_h_C = np.random.binomial(1, TOM_H_PROB, size=(1,8))
+	micro_rhythm_tom_h_D = np.random.binomial(1, TOM_H_PROB, size=(1,8))
+
+	base_rhythm_tom_h_A = np.concatenate((micro_rhythm_tom_h_A, micro_rhythm_tom_h_A), axis=1)
+	base_rhythm_tom_h_B = np.concatenate((micro_rhythm_tom_h_B, micro_rhythm_tom_h_B), axis=1)
+	base_rhythm_tom_h_C = np.concatenate((micro_rhythm_tom_h_C, micro_rhythm_tom_h_C), axis=1)
+	base_rhythm_tom_h_D = np.concatenate((micro_rhythm_tom_h_D, micro_rhythm_tom_h_D), axis=1)
+
+	micro_rhythm_tom_m_A = np.random.binomial(1, TOM_M_PROB, size=(1,8))
+	micro_rhythm_tom_m_B = np.random.binomial(1, TOM_M_PROB, size=(1,8))
+	micro_rhythm_tom_m_C = np.random.binomial(1, TOM_M_PROB, size=(1,8))
+	micro_rhythm_tom_m_D = np.random.binomial(1, TOM_M_PROB, size=(1,8))
+
+	base_rhythm_tom_m_A = np.concatenate((micro_rhythm_tom_m_A, micro_rhythm_tom_m_A), axis=1)
+	base_rhythm_tom_m_B = np.concatenate((micro_rhythm_tom_m_B, micro_rhythm_tom_m_B), axis=1)
+	base_rhythm_tom_m_C = np.concatenate((micro_rhythm_tom_m_C, micro_rhythm_tom_m_C), axis=1)
+	base_rhythm_tom_m_D = np.concatenate((micro_rhythm_tom_m_D, micro_rhythm_tom_m_D), axis=1)
+
+	micro_rhythm_tom_l_A = np.random.binomial(1, TOM_L_PROB, size=(1,8))
+	micro_rhythm_tom_l_B = np.random.binomial(1, TOM_L_PROB, size=(1,8))
+	micro_rhythm_tom_l_C = np.random.binomial(1, TOM_L_PROB, size=(1,8))
+	micro_rhythm_tom_l_D = np.random.binomial(1, TOM_L_PROB, size=(1,8))
+
+	base_rhythm_tom_l_A = np.concatenate((micro_rhythm_tom_l_A, micro_rhythm_tom_l_A), axis=1)
+	base_rhythm_tom_l_B = np.concatenate((micro_rhythm_tom_l_B, micro_rhythm_tom_l_B), axis=1)
+	base_rhythm_tom_l_C = np.concatenate((micro_rhythm_tom_l_C, micro_rhythm_tom_l_C), axis=1)
+	base_rhythm_tom_l_D = np.concatenate((micro_rhythm_tom_l_D, micro_rhythm_tom_l_D), axis=1)
+
+else: 
+
+	base_rhythm_tom_h_A = np.random.binomial(1, TOM_H_PROB, size=(1,16))
+	base_rhythm_tom_h_B = np.random.binomial(1, TOM_H_PROB, size=(1,16))
+	base_rhythm_tom_h_C = np.random.binomial(1, TOM_H_PROB, size=(1,16))
+	base_rhythm_tom_h_D = np.random.binomial(1, TOM_H_PROB, size=(1,16))
+
+	base_rhythm_tom_m_A = np.random.binomial(1, TOM_M_PROB, size=(1,16))
+	base_rhythm_tom_m_B = np.random.binomial(1, TOM_M_PROB, size=(1,16))
+	base_rhythm_tom_m_C = np.random.binomial(1, TOM_M_PROB, size=(1,16))
+	base_rhythm_tom_m_D = np.random.binomial(1, TOM_M_PROB, size=(1,16))
+
+	base_rhythm_tom_l_A = np.random.binomial(1, TOM_L_PROB, size=(1,16))
+	base_rhythm_tom_l_B = np.random.binomial(1, TOM_L_PROB, size=(1,16))
+	base_rhythm_tom_l_C = np.random.binomial(1, TOM_L_PROB, size=(1,16))
+	base_rhythm_tom_l_D = np.random.binomial(1, TOM_L_PROB, size=(1,16))
+
+base_fill_hihat_A = np.random.binomial(2, FILL_HIHAT_PROB, size=(1,FILL_LENGTH))
+base_fill_hihat_B = np.random.binomial(2, FILL_HIHAT_PROB, size=(1,FILL_LENGTH))
+base_fill_hihat_C = np.random.binomial(2, FILL_HIHAT_PROB, size=(1,FILL_LENGTH))
+base_fill_hihat_D = np.random.binomial(2, FILL_HIHAT_PROB, size=(1,FILL_LENGTH))
+
+base_fill_snare_A = np.random.binomial(1, FILL_SNARE_PROB, size=(1,FILL_LENGTH))
+base_fill_snare_B = np.random.binomial(1, FILL_SNARE_PROB, size=(1,FILL_LENGTH))
+base_fill_snare_C = np.random.binomial(1, FILL_SNARE_PROB, size=(1,FILL_LENGTH))
+base_fill_snare_D = np.random.binomial(1, FILL_SNARE_PROB, size=(1,FILL_LENGTH))
+
+base_fill_tom_H_A = np.random.binomial(1, FILL_TOM_H_PROB, size=(1,FILL_LENGTH))
+base_fill_tom_H_B = np.random.binomial(1, FILL_TOM_H_PROB, size=(1,FILL_LENGTH))
+base_fill_tom_H_C = np.random.binomial(1, FILL_TOM_H_PROB, size=(1,FILL_LENGTH))
+base_fill_tom_H_D = np.random.binomial(1, FILL_TOM_H_PROB, size=(1,FILL_LENGTH))
+
+base_fill_tom_M_A = np.random.binomial(1, FILL_TOM_M_PROB, size=(1,FILL_LENGTH))
+base_fill_tom_M_B = np.random.binomial(1, FILL_TOM_M_PROB, size=(1,FILL_LENGTH))
+base_fill_tom_M_C = np.random.binomial(1, FILL_TOM_M_PROB, size=(1,FILL_LENGTH))
+base_fill_tom_M_D = np.random.binomial(1, FILL_TOM_M_PROB, size=(1,FILL_LENGTH))
+
+base_fill_tom_L_A = np.random.binomial(1, FILL_TOM_L_PROB, size=(1,FILL_LENGTH))
+base_fill_tom_L_B = np.random.binomial(1, FILL_TOM_L_PROB, size=(1,FILL_LENGTH))
+base_fill_tom_L_C = np.random.binomial(1, FILL_TOM_L_PROB, size=(1,FILL_LENGTH))
+base_fill_tom_L_D = np.random.binomial(1, FILL_TOM_L_PROB, size=(1,FILL_LENGTH))
+
+base_fill_kick_A = np.random.binomial(1, FILL_KICK_PROB, size=(1,FILL_LENGTH))
+base_fill_kick_B = np.random.binomial(1, FILL_KICK_PROB, size=(1,FILL_LENGTH))
+base_fill_kick_C = np.random.binomial(1, FILL_KICK_PROB, size=(1,FILL_LENGTH))
+base_fill_kick_D = np.random.binomial(1, FILL_KICK_PROB, size=(1,FILL_LENGTH))
+
+base_fill_crash_A = np.random.binomial(1, FILL_CRASH_PROB, size=(1,FILL_LENGTH))
+base_fill_crash_B = np.random.binomial(1, FILL_CRASH_PROB, size=(1,FILL_LENGTH))
+base_fill_crash_C = np.random.binomial(1, FILL_CRASH_PROB, size=(1,FILL_LENGTH))
+base_fill_crash_D = np.random.binomial(1, FILL_CRASH_PROB, size=(1,FILL_LENGTH))
+
+rhythm_hihat_A = np.concatenate((base_rhythm_hihat_A, base_rhythm_hihat_A, base_rhythm_hihat_A, np.concatenate((base_rhythm_hihat_A[0, 0:17-FILL_LENGTH], base_fill_hihat_A[0]), axis=0)[np.newaxis]), axis=1)
+rhythm_hihat_B = np.concatenate((base_rhythm_hihat_B, base_rhythm_hihat_B, base_rhythm_hihat_B, np.concatenate((base_rhythm_hihat_B[0, 0:17-FILL_LENGTH], base_fill_hihat_B[0]), axis=0)[np.newaxis]), axis=1)
+rhythm_hihat_C = np.concatenate((base_rhythm_hihat_C, base_rhythm_hihat_C, base_rhythm_hihat_C, np.concatenate((base_rhythm_hihat_C[0, 0:17-FILL_LENGTH], base_fill_hihat_C[0]), axis=0)[np.newaxis]), axis=1)
+rhythm_hihat_D = np.concatenate((base_rhythm_hihat_D, base_rhythm_hihat_D, base_rhythm_hihat_D, np.concatenate((base_rhythm_hihat_D[0, 0:17-FILL_LENGTH], base_fill_hihat_D[0]), axis=0)[np.newaxis]), axis=1)
+
+rhythm_tom_h_A = np.concatenate((base_rhythm_tom_h_A, base_rhythm_tom_h_A, base_rhythm_tom_h_A, np.concatenate((base_rhythm_tom_h_A[0, 0:17-FILL_LENGTH], base_fill_tom_H_A[0]), axis=0)[np.newaxis]), axis=1)
+rhythm_tom_h_B = np.concatenate((base_rhythm_tom_h_B, base_rhythm_tom_h_B, base_rhythm_tom_h_B, np.concatenate((base_rhythm_tom_h_B[0, 0:17-FILL_LENGTH], base_fill_tom_H_B[0]), axis=0)[np.newaxis]), axis=1)
+rhythm_tom_h_C = np.concatenate((base_rhythm_tom_h_C, base_rhythm_tom_h_C, base_rhythm_tom_h_C, np.concatenate((base_rhythm_tom_h_C[0, 0:17-FILL_LENGTH], base_fill_tom_H_C[0]), axis=0)[np.newaxis]), axis=1)
+rhythm_tom_h_D = np.concatenate((base_rhythm_tom_h_D, base_rhythm_tom_h_D, base_rhythm_tom_h_D, np.concatenate((base_rhythm_tom_h_D[0, 0:17-FILL_LENGTH], base_fill_tom_H_D[0]), axis=0)[np.newaxis]), axis=1)
+
+rhythm_tom_m_A = np.concatenate((base_rhythm_tom_m_A, base_rhythm_tom_m_A, base_rhythm_tom_m_A, np.concatenate((base_rhythm_tom_m_A[0, 0:17-FILL_LENGTH], base_fill_tom_M_A[0]), axis=0)[np.newaxis]), axis=1)
+rhythm_tom_m_B = np.concatenate((base_rhythm_tom_m_B, base_rhythm_tom_m_B, base_rhythm_tom_m_B, np.concatenate((base_rhythm_tom_m_B[0, 0:17-FILL_LENGTH], base_fill_tom_M_B[0]), axis=0)[np.newaxis]), axis=1)
+rhythm_tom_m_C = np.concatenate((base_rhythm_tom_m_C, base_rhythm_tom_m_C, base_rhythm_tom_m_C, np.concatenate((base_rhythm_tom_m_C[0, 0:17-FILL_LENGTH], base_fill_tom_M_C[0]), axis=0)[np.newaxis]), axis=1)
+rhythm_tom_m_D = np.concatenate((base_rhythm_tom_m_D, base_rhythm_tom_m_D, base_rhythm_tom_m_D, np.concatenate((base_rhythm_tom_m_D[0, 0:17-FILL_LENGTH], base_fill_tom_M_D[0]), axis=0)[np.newaxis]), axis=1)
+
+rhythm_tom_l_A = np.concatenate((base_rhythm_tom_l_A, base_rhythm_tom_l_A, base_rhythm_tom_l_A, np.concatenate((base_rhythm_tom_l_A[0, 0:17-FILL_LENGTH], base_fill_tom_L_A[0]), axis=0)[np.newaxis]), axis=1)
+rhythm_tom_l_B = np.concatenate((base_rhythm_tom_l_B, base_rhythm_tom_l_B, base_rhythm_tom_l_B, np.concatenate((base_rhythm_tom_l_B[0, 0:17-FILL_LENGTH], base_fill_tom_L_B[0]), axis=0)[np.newaxis]), axis=1)
+rhythm_tom_l_C = np.concatenate((base_rhythm_tom_l_C, base_rhythm_tom_l_C, base_rhythm_tom_l_C, np.concatenate((base_rhythm_tom_l_C[0, 0:17-FILL_LENGTH], base_fill_tom_L_C[0]), axis=0)[np.newaxis]), axis=1)
+rhythm_tom_l_D = np.concatenate((base_rhythm_tom_l_D, base_rhythm_tom_l_D, base_rhythm_tom_l_D, np.concatenate((base_rhythm_tom_l_D[0, 0:17-FILL_LENGTH], base_fill_tom_L_D[0]), axis=0)[np.newaxis]), axis=1)
+
+rhythm_snare_A = np.concatenate((base_rhythm_snare_A, base_rhythm_snare_A, base_rhythm_snare_A, np.concatenate((base_rhythm_snare_A[0, 0:17-FILL_LENGTH], base_fill_snare_A[0]), axis=0)[np.newaxis]), axis=1)
+rhythm_snare_B = np.concatenate((base_rhythm_snare_B, base_rhythm_snare_B, base_rhythm_snare_B, np.concatenate((base_rhythm_snare_B[0, 0:17-FILL_LENGTH], base_fill_snare_B[0]), axis=0)[np.newaxis]), axis=1)
+rhythm_snare_C = np.concatenate((base_rhythm_snare_C, base_rhythm_snare_C, base_rhythm_snare_C, np.concatenate((base_rhythm_snare_C[0, 0:17-FILL_LENGTH], base_fill_snare_C[0]), axis=0)[np.newaxis]), axis=1)
+rhythm_snare_D = np.concatenate((base_rhythm_snare_D, base_rhythm_snare_D, base_rhythm_snare_D, np.concatenate((base_rhythm_snare_D[0, 0:17-FILL_LENGTH], base_fill_snare_D[0]), axis=0)[np.newaxis]), axis=1)
+
+rhythm_kick_A = np.concatenate((base_rhythm_kick_A, base_rhythm_kick_A, base_rhythm_kick_A, np.concatenate((base_rhythm_kick_A[0, 0:17-FILL_LENGTH], base_fill_kick_A[0]), axis=0)[np.newaxis]), axis=1)
+rhythm_kick_B = np.concatenate((base_rhythm_kick_B, base_rhythm_kick_B, base_rhythm_kick_B, np.concatenate((base_rhythm_kick_B[0, 0:17-FILL_LENGTH], base_fill_kick_B[0]), axis=0)[np.newaxis]), axis=1)
+rhythm_kick_C = np.concatenate((base_rhythm_kick_C, base_rhythm_kick_C, base_rhythm_kick_C, np.concatenate((base_rhythm_kick_C[0, 0:17-FILL_LENGTH], base_fill_kick_C[0]), axis=0)[np.newaxis]), axis=1)
+rhythm_kick_D = np.concatenate((base_rhythm_kick_D, base_rhythm_kick_D, base_rhythm_kick_D, np.concatenate((base_rhythm_kick_D[0, 0:17-FILL_LENGTH], base_fill_kick_D[0]), axis=0)[np.newaxis]), axis=1)
+
+
+print(rhythm_hihat_A)
+print(rhythm_tom_h_A)
+print(rhythm_tom_m_A)
+print(rhythm_tom_l_A)
+print(rhythm_snare_A)
+print(rhythm_kick_A)
+print("----")
+
 
 
